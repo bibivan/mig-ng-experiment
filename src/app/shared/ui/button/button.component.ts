0c9b6aaec1653778829a0bc66cb1d486164b5868
Mikhail Safronov <saforov@internetlab.ru>
2021-03-30 16:34:01 +0400

    fix

--- src/app/shared/ui/button/button.component.ts
+++ src/app/shared/ui/button/button.component.ts
@@ -7,7 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core'
 })
 export class ButtonComponent {
   @Input() type: 'button' | 'submit' = 'button'
-  @Input() theme: 'default' | 'primary' | 'get-money' | 'return' = 'primary'
+  @Input() theme: 'default' | 'primary' | 'get-money' | 'cancel' | 'return' = 'primary'
   @Input() inline: boolean
   @Input() disabled: boolean
 
