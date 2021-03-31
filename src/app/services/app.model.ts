7fc412aabea9e8cd64b00d925ce8cbfab94f561b
Maksimov Aleksey <almax@internetlab.ru>
2021-03-31 18:47:46 +0400

    в процессе

---
+++ src/app/services/app.model.ts
@@ -0,0 +1,5 @@
+export interface AppStateInterface {
+  isInit: boolean,
+  isLoading: boolean,
+  page: 'string'
+}
--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core'
+import { AppStateInterface } from './app.model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
+  private state: AppStateInterface = {
+    isInit: false,
+    isLoading: true,
+    page: null
+  }
 
   constructor() { }
 }
